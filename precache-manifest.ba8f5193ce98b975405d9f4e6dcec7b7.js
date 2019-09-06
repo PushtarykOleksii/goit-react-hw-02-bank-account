59df37056970e332c9468941222672b1904d7cb1
Pushtaryk Oleksii <pushtarik@gmail.come>
2019-09-06 08:10:30 +0300

    Updates

---
+++ precache-manifest.ba8f5193ce98b975405d9f4e6dcec7b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faad79d1f256f51425d41825ddd7d1cb",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "960c7946bd75e4877ff4",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "11e41d5400b0ec7f47fa",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.61717d44.chunk.css"
+  },
+  {
+    "revision": "960c7946bd75e4877ff4",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.9e0b7f7c.chunk.js"
+  },
+  {
+    "revision": "11e41d5400b0ec7f47fa",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.f93e9139.chunk.js"
+  },
+  {
+    "revision": "cd7c256155f9807b196b",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime~main.c0c630de.js"
+  }
+]);
